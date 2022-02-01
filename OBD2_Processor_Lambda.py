31505c52e21063c62a2bd3bb38affd229da50eb1
Adam Alkhafaji <76083769+AdamAlkhafaji96@users.noreply.github.com>
2022-01-31 20:58:27 -0600

    Update OBD2_Processor_Lambda.py

--- OBD2_Processor_Lambda.py
+++ OBD2_Processor_Lambda.py
@@ -1,13 +1,12 @@
+from datetime import datetime
 import awswrangler as wr
 import boto3
-import bs4
 from bs4 import BeautifulSoup
 import pandas as pd
-from datetime import datetime
 
 def html_to_parquet():
     """
-    Args: raw html files read from s3 -> 'obd-diagnostic-data-input'
+    Args: raw html files read from s3 <- 'obd-diagnostic-data-input'
     Using: bs4, pandas, boto3, awswrangler
     Returns: .parquet files to s3 ->'obd-diagnostic-data-output'
     """
@@ -16,7 +15,7 @@ def html_to_parquet():
     s3 = boto3.client('s3')
     
     raw_bucket = 'obd-diagnostic-data-input'
-    key = 'Diagnostic Report Toyota Camry 2018 1_30_2022.html'
+    key = 'Diagnostic Report Honda Pilot 2017 1_21_2022.html'    
     
     pandas_response = s3.get_object(Bucket=raw_bucket, Key=key)
     soup_response = s3.get_object(Bucket=raw_bucket, Key=key)
@@ -31,13 +30,12 @@ def html_to_parquet():
     b = soup.b.parent.text.split() 
     
     vals = {
-        'Time': 'NULL', 
-        'VIN': 'NULL',
-        'Manufacturer': 'NULL',
-        'Model': 'NULL',
-        'Option': 'NULL',
-        'Year': 'NULL'
-    }
+        'Time': '', 
+        'VIN': '',
+        'Manufacturer': '',
+        'Model': '',
+        'Option': '',
+        'Year': ''}
 
     for i in range(len(b) - 1):
         if b[i] == 'Date:':
@@ -65,33 +63,21 @@ def html_to_parquet():
             vals['Year'] = b[i+1]
     
     vehicle_df = pd.DataFrame(vals, 
-                              columns=['Time', 
-                                       'VIN', 
-                                       'Manufacturer', 
-                                       'Model', 
-                                       'Option', 
-                                       'Year'], 
-                              index=[0]
-    )
+                              columns=['Time', 'VIN', 'Manufacturer', 
+                                       'Model', 'Option', 'Year'], 
+                              index=[0])
     
     file_date = date_obj.strftime('%Y_%m_%d')
 
-    file_name = '{}_{}_{}/VIN: {}/{}/{}'.format(vals['Manufacturer'],
-                                                vals['Model'], 
-                                                vals['Year'],
-                                                vals['VIN'], 
-                                                'vehicle_report_info',
-                                                file_date
-    )  
-    
-    path_1 = 's3://obd-diagnostic-data-output/{}'.format(file_name)
+    file_name = f"{vals['Manufacturer']}_{vals['Model']}_{vals['Year']}/VIN: {vals['VIN']}/{'vehicle_report_info'}/{file_date}"
+
+    path_1 = f"s3://obd-diagnostic-data-output/{file_name}"
     
     wr.s3.to_parquet(df=vehicle_df,
                     path=path_1,
                     compression='gzip',
                     dataset=True,
-                    mode='append'
-    )
+                    mode='append')
     
     frames = {
         'software_info': True,
@@ -103,61 +89,55 @@ def html_to_parquet():
         'oxygen_sensors': True,
         'on_board_monitoring': True,
         'general_info': True,
-        'in_performance_tracking': True
-    }
+        'in_performance_tracking': True}
     
     for i in range(len(df_list) - 1):
         
         software_df = df_list[0]
         software_df.rename(columns={0: "Name", 1: "Website"}, inplace=True)
+        df_list[0] = software_df
             
         check = soup.br.parent.text
         
-        target = 'There are no pending, stored, or permanent diagnostic trouble codes (DTCs).'
-        for t in range(len(check)):
-            if check[t:t+75] == target:
+        target = 'There are no pending, stored, or \
+                permanent diagnostic trouble codes (DTCs).'
+        for idx, _ in enumerate(check):
+            if check[idx:idx+75] == target:
                 frames['trouble_codes'] = False
                 frames['additional_info'] = False
                     
         target_2 = 'Freeze Frame data is not available.'
-        for f in range(len(check)):
-            if check[f:f+35] == target_2:
+        for idx, _ in enumerate(check):
+            if check[idx:idx+35] == target_2:
                 frames['freeze_frame_data'] = False
 
         if len(df_list[i]) == 0:
             frames['oxygen_sensors'] = False
-            del(df_list[i])
+            del df_list[i]
                 
         target_3 = 'On-Board Monitoring data is not available'
-        for o in range(len(check)):
-            if check[o:o+41] == target_3:
+        for idx, _ in enumerate(check):
+            if check[idx:idx+41] == target_3:
                 frames['on_board_monitoring'] = False
             
         target_4 = 'In-Performance Tracking'
         h3 = soup('h3')
-        check_4 = [h3[element].text for element in range(len(h3))]
+        check_4 = [h3[element].text for element, _ in enumerate(h3)]
         if target_4 not in check_4:
             frames['in_performance_tracking'] = False
         
-        frame_exists = [key for key, val in frames.items() if val == True]
+        frame_exists = [key for key, val in frames.items() if val is True]
 
-    for index in range(len(df_list)):
-            
-        file_names = '{}_{}_{}/VIN: {}/{}/{}'.format(vals['Manufacturer'],
-                                                     vals['Model'], 
-                                                     vals['Year'], 
-                                                     vals['VIN'],
-                                                     frame_exists[index], 
-                                                     file_date
-        )
+    for index, _ in enumerate(df_list):
 
-        path_2 = 's3://obd-diagnostic-data-output/{}'.format(file_names)
+        file_names = f"{vals['Manufacturer']}_{vals['Model']}_{vals['Year']}/VIN: {vals['VIN']}/{frame_exists[index]}/{file_date}"
+
+        path_2 = f"s3://obd-diagnostic-data-output/{file_names}"
 
         wr.s3.to_parquet(df=df_list[index],
-                         path=path_2,
+                         path=path_2, 
                          compression='gzip',
                          dataset=True,
-                         mode='append'
-        )
+                         mode='append')
         
-    return 'Etl Success for {} {} {}'.format(vals['Manufacturer'], vals['Model'], vals['Year'])
+    return f"Etl Success for {vals['Manufacturer']} {vals['Model']} {vals['Year']}"
